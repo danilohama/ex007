f54b2a99024c20a5123d55e5af34a35c9808d740
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-04 19:41:46 -0300

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, e calcule a sua média.
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(' Nota 1 = {} \n Nota 2 = {}\n Média final = {}'.format(nota1, nota2, media))
